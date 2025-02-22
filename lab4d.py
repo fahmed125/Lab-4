16b373f30f96740c274fcbeef8b145069bd5651e
Fuad Ahmed <fuadahmed.can@outlook.com>
2025-02-21 20:06:43 -0500

    Lab4

---
+++ lab4d.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Strings 1
+
+str1 = 'Hello World!!'
+str2 = 'Seneca College'
+
+num1 = 1500
+num2 = 1.50
+
+def first_five(one):
+    # Place code here - refer to function specifics in section below
+	return one[:5]
+def last_seven(two):
+    # Place code here - refer to function specifics in section below
+	return two[-7:]
+def middle_number(number):
+    # Place code here - refer to function specifics in section below
+	str_num= str(number)
+	return str_num[1:3] if len(str_num)>2 else str_num
+def first_three_last_three(first,last):
+    # Place code here - refer to function specifics in section below
+	return first[:3] + last[-3:]
+
+if __name__ == '__main__':
+    print(first_five(str1))
+    print(first_five(str2))
+    print(last_seven(str1))
+    print(last_seven(str2))
+    print(middle_number(num1))
+    print(middle_number(num2))
+    print(first_three_last_three(str1, str2))
+    print(first_three_last_three(str2, str1))
